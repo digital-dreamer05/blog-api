860b9b7b006a8747ddb9984a21d1444116477ddd
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-08-02 16:14:24 +0330

    feat(models): create article model

---
+++ models/Article.js
@@ -0,0 +1,38 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const Article = (sequelize) =>
+  sequelize.define(
+    'article',
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      slug: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      cover: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: 'articles',
+      timestamps: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    }
+  );
+
+module.exports = Article;
