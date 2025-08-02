4be0d53ad20683ccb8c1ebd5719d0ea519169bb8
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-08-02 16:28:13 +0330

    feat(models): create tags_articles model

---
+++ models/TagsArticles.js
@@ -0,0 +1,8 @@
+const TagsArticles = (sequelize) =>
+  sequelize.define(
+    'tags_articles',
+    {},
+    { tableName: 'tags_articles', timestamps: false }
+  );
+
+module.exports = TagsArticles;
