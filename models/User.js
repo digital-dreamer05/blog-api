23d42c7fd4d3089574367415cd99f7c39dce3fab
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-08-02 14:52:46 +0330

    feat(models): create user model

---
+++ models/User.js
@@ -0,0 +1,55 @@
+const { DataTypes } = require('sequelize');
+const { create } = require('svg-captcha');
+
+const User = (sequelize) => {
+  return sequelize.define(
+    'user',
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      avatar: {
+        type: DataTypes.STRING,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      role: {
+        type: DataTypes.ENUM,
+        values: ['user', 'admin'],
+        allowNull: true,
+        defaultValue: 'user',
+      },
+      provider: {
+        type: DataTypes.ENUM,
+        values: ['local', 'google'],
+        allowNull: true,
+        defaultValue: 'local',
+      },
+    },
+    {
+      tableName: 'users',
+      timestamps: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    }
+  );
+};
+
+module.exports = User;
