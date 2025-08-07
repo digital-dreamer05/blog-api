e83d9a6e661ac72b661128b3b29fa9dd7c2d8be1
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-08-07 21:02:56 +0330

    docs(db): add JSDoc comments to db.js

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require('sequelize');
 const configs = require('./configs');
 
+const Tag = require('./models/Tag');
+const TagsArticles = require('./models/TagsArticles');
+const Article = require('./models/Article');
+
 const db = new Sequelize({
   host: configs.db.host,
   port: configs.db.port,
@@ -9,6 +13,46 @@ const db = new Sequelize({
   database: configs.db.name,
   dialect: configs.db.dialect,
   logging: configs.isProduction ? false : console.log,
+});
+
+//* JsDoc
+/**
+ * @typedef {import('sequelize').ModelCtor<import('sequelize').Model<any, any>>}
+ */
+const User = require('./models/User')(db);
+
+/**
+ * @typedef {import('sequelize').ModelCtor<import('sequelize').Model<any, any>>}
+ */
+const Tag = require('./models/Tag')(db);
+
+/**
+ * @typedef {import('sequelize').ModelCtor<import('sequelize').Model<any, any>>}
+ */
+const TagsArticles = require('./models/TagsArticles')(db);
+
+/**
+ * @typedef {import('sequelize').ModelCtor<import('sequelize').Model<any, any>>}
+ */
+const Article = require('./models/Article')(db);
+
+User.hasMany(Article, {
+  foreignKey: 'author_id',
+  onDelete: 'CASCADE',
+});
+
+Article.belongsTo(User, { foreignKey: 'author_id', as: 'author' });
+
+Article.belongsToMany(Tag, {
+  through: TagsArticles,
+  onDelete: 'CASCADE',
+  foreignKey: 'article_id',
+});
+
+Tag.belongsToMany(Article, {
+  through: TagsArticles,
+  onDelete: 'CASCADE',
+  foreignKey: 'tag_id',
 });
 
 module.exports = db;
