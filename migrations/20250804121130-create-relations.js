31ef198571723fdefd61fc68d4ce01f8194e0dcf
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-08-07 20:59:21 +0330

    refactor(migrations): update relationships between models

--- migrations/20250804121130-create-relations.js
+++ migrations/20250804121130-create-relations.js
@@ -2,27 +2,68 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Relations', {
-      id: {
+    const transcation = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.addColumn('articles', 'author_id', {
+        type: Sequelize.INTEGER.UNSIGNED,
         allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+
+        onDelete: 'CASCADE',
+      });
+
+      await queryInterface.createTable('tags_articles', {
+        article_id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+          references: {
+            model: 'articles',
+            key: 'id',
+          },
+
+          onDelete: 'CASCADE',
+        },
+
+        tag_id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+          references: {
+            model: 'tags',
+            key: 'id',
+          },
+
+          onDelete: 'CASCADE',
+        },
+      });
+
+      await queryInterface.addConstraint('tags_articles', {
+        fields: ['article_id', 'tag_id'],
+        type: 'unique',
+        name: 'unique_article_tag',
+      });
+
+      await transcation.commit();
+    } catch (err) {
+      await transcation.rollback();
+      throw err;
+    }
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Relations');
-  }
+  async down(queryInterface) {
+    const transcation = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.removeColumn('articles', 'author_id');
+      await queryInterface.dropTable('tags_articles');
+      await queryInterface.commit();
+    } catch (err) {
+      await transcation.rollback();
+      throw err;
+    }
+
+    await queryInterface.dropTable('relations');
+  },
 };
