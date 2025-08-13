83d34a6ec69c5a778093a4dbda174392181ce6ab
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-08-13 16:45:59 +0330

    chore(server): update server.js configuration

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const { db } = require('./db');
+const app = require('./app');
+const config = require('./configs');
+const redis = require('./redis');
+
+async function startServer() {
+  try {
+    await db.authenticate();
+    await redis.ping();
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}...`);
+    });
+  } catch (err) {
+    console.error('Error -->', err);
+
+    await db.close();
+    await redis.disconnect();
+  }
+}
+
+startServer();
