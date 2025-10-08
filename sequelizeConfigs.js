5be7e51a826b99c369f8ff5e3b3ee12b4132f61b
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-10-08 19:27:18 +0330

    chore(config): update sequelizeConfigs.js settings

--- sequelizeConfigs.js
+++ sequelizeConfigs.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const configs = require('./configs');
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -6,23 +8,38 @@ module.exports = {
     password: configs.db.password,
     database: configs.db.name,
     host: configs.db.host,
-    dialect: configs.db.dialect,
     port: configs.db.port,
+    dialect: configs.db.dialect,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(process.env.DB_SSL_CA_PATH).toString(),
+      },
+    },
   },
   test: {
     username: configs.db.user,
     password: configs.db.password,
     database: configs.db.name,
     host: configs.db.host,
-    dialect: configs.db.dialect,
     port: configs.db.port,
+    dialect: configs.db.dialect,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(process.env.DB_SSL_CA_PATH).toString(),
+      },
+    },
   },
   production: {
     username: configs.db.user,
     password: configs.db.password,
     database: configs.db.name,
     host: configs.db.host,
-    dialect: configs.db.dialect,
     port: configs.db.port,
+    dialect: configs.db.dialect,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(process.env.DB_SSL_CA_PATH).toString(),
+      },
+    },
   },
 };
