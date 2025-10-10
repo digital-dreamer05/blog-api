c4256068fbc5d11007011161ea61c9484f1922ee
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-10-10 11:39:59 +0330

    chore: remove frontend directories from project

--- blog-frontend/assets/js/config.js
+++
@@ -1,9 +0,0 @@
-// Frontend Config
-window.__API_CONFIG__ = {
-  baseUrl: 'https://blog-api-mw5f.onrender.com',
-};
-
-// App base path (prefix for all internal links). For root, set to '' or '/'
-window.__APP_CONFIG__ = {
-  basePath: '/blog-frontend',
-};
--- blog-frontend/js/config.js
+++
@@ -1,5 +0,0 @@
-// Frontend Config
-// Set this to your backend URL in production deploy
-window.__API_CONFIG__ = {
-  baseUrl: "https://blog-api-mw5f.onrender.com/", // e.g. 'https://your-backend.onrender.com'
-};
